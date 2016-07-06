fe177c1893d1b7a6451883568eeda74192411ba3
TomYanovich@gmail.com <TomYanovich@gmail.com>
2016-07-06 12:30:51 +0300

    1st commit

--- Visualization/src/main/Main.java
+++ Visualization/src/main/Main.java
@@ -1,16 +1,12 @@
 package main;
+
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
-	
-	private static final int NUM_OF_EPISODES = 3;
-	
+
 	public static void main(String[] args) throws IOException {
-		
-		List<String> longestEpisodes = WordMatrix.getKlongestEpisodes(NUM_OF_EPISODES);
-		for (String episode_id : longestEpisodes){
-			WordMatrix.init(episode_id).fill().exportTSV();
-		}
+
+		WordMatrix.init("Israel", Utils.DOMESTIC).fill().exportTSV();
+
 	}
 }
--- Visualization/src/main/WordMatrix.java
+++ Visualization/src/main/WordMatrix.java
@@ -4,15 +4,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DateFormat;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -24,19 +25,19 @@ import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bson.types.ObjectId;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 
 public class WordMatrix {
 
 	private static final int NUM_OF_KEYWORDS = 40;
-
 	/*
 	 * rows = dates, columns = words, values = frequencies
 	 */
@@ -46,13 +47,23 @@ public class WordMatrix {
 	 */
 	private int[] wordFrequencies;
 
-	private final Date start_date;
+	private Calendar start_date;
 
-	private final Date end_date;
+	private Calendar end_date;
 
 	private ArrayList<String> keywords;
 
-	private String episode_id;
+	private String name;
+
+	private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyyMMdd");
+
+	private static HashMap<String, HashSet<String>> event_keywords = new HashMap<>();
+
+	private static HashMap<String, String> domains = new HashMap<>();
+
+	private static String country;
+
+	private static boolean foreignOrDomestic;
 
 	/**
 	 * words total number of words document = total number of documents
@@ -60,44 +71,50 @@ public class WordMatrix {
 	 * @param words
 	 * @param documents
 	 */
-	public WordMatrix(String id, Date start, Date end, Collection<String> words) {
+	public WordMatrix(Calendar start, Calendar end, Collection<String> words) {
 		super();
-		episode_id = id;
 		start_date = start;
 		end_date = end;
-		keywords = new ArrayList<String>(words);
+		this.keywords = new ArrayList<String>(words);
 		Collections.sort(keywords);
-		wordMatrix = new int[getDateDiff(start, end, TimeUnit.DAYS) + 1][keywords.size()];
-		wordFrequencies = new int[keywords.size()];
+		this.wordMatrix = new int[Utils.getDateDiff(start, end, TimeUnit.DAYS) + 1][keywords.size()];
+		this.wordFrequencies = new int[keywords.size()];
+
+		this.name = dateFormatter.format(start_date.getTime()) + "-" + dateFormatter.format(end_date.getTime());
 	}
 
 	public int[][] getWordMatrix() {
 		return wordMatrix;
 	}
 
-	public boolean insert(Date date, String keyword, int f) {
-		if (date.before(start_date) || date.after(end_date) || !keywords.contains(keyword) || f < 0) {
+	public synchronized boolean insert(Calendar date, String keyword, int f) {
+		if (date.before(start_date) || date.after(end_date) || f <= 0) {
 			return false;
 		}
+
 		int wordIndex = keywords.indexOf(keyword);
 		wordMatrix[getIndexOfDate(date)][wordIndex] += f;
 		wordFrequencies[wordIndex] += f;
 		return true;
 	}
 
-	public int getf(Date date, String keyword) {
+	public int getf(Calendar date, String keyword) {
 		return wordMatrix[getIndexOfDate(date)][keywords.indexOf(keyword)];
+	}
+
+	public int getf(int row, String keyword) {
+		return wordMatrix[row][keywords.indexOf(keyword)];
 	}
 
 	public int getWordFrequency(String keyword) {
 		return wordFrequencies[keywords.indexOf(keyword)];
 	}
 
-	private int getIndexOfDate(Date date) {
+	private int getIndexOfDate(Calendar date) {
 		if (date.before(start_date)) {
 			return -1;
 		}
-		return getDateDiff(start_date, date, TimeUnit.DAYS);
+		return Utils.getDateDiff(start_date, date, TimeUnit.DAYS);
 	}
 
 	private TreeSet<String> sortByFrequency() {
@@ -118,27 +135,15 @@ public class WordMatrix {
 		return sortedKeywords;
 	}
 
-	public String getEpisodeId() {
-		return episode_id;
-	}
-
 	public List<String> getKeywords() {
 		return keywords;
 	}
 
-	private static int getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
-		long diffInMillies = date2.getTime() - date1.getTime();
-		return (int) timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS);
-	}
-
-	public static Date convertStrToDate(String date) {
-		try {
-			DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-			return df.parse(date.substring(0, 10));
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return null;
-		}
+	private String getDateOfRow(int row, SimpleDateFormat formatter) {
+		Calendar dateOfRow = Calendar.getInstance();
+		dateOfRow.setTimeInMillis(start_date.getTimeInMillis());
+		dateOfRow.add(Calendar.DATE, row);
+		return formatter.format(dateOfRow.getTime());
 	}
 
 	public static List<String> getKlongestEpisodes(int k) {
@@ -160,11 +165,12 @@ public class WordMatrix {
 			String _id = "";
 			try {
 				_id = cursor.curr().get("_id").toString();
-				Date start_date = convertStrToDate((cursor.curr().get("rssUpdFirst")).toString());
-				Date end_date = convertStrToDate((cursor.curr().get("rssUpdLast")).toString());
-				int duration = getDateDiff(start_date, end_date, TimeUnit.DAYS);
+				Calendar start_date = Utils.parseDateISO8601((cursor.curr().get("rssUpdFirst")).toString());
+				Calendar end_date = Utils.parseDateISO8601((cursor.curr().get("rssUpdLast")).toString());
+				int duration = Utils.getDateDiff(start_date, end_date, TimeUnit.DAYS);
 				episodesMap.put(_id, duration);
-			} catch (NullPointerException e) {
+			} catch (Exception e) {
+				e.printStackTrace();
 				continue;
 			}
 
@@ -178,69 +184,215 @@ public class WordMatrix {
 		return longestEpisodes;
 	}
 
-	public static WordMatrix init(String episode_id) {
-		HashSet<String> keyWordSet = null;
-		BasicDBObject query = new BasicDBObject("_id", new ObjectId(episode_id));
-		DBCollection coll = MyDB.getDB().getCollection("newsItems_Episodes");
+	public static WordMatrix init(String _country, boolean _foreignOrDomestic) {
+		WordMatrix.country = _country;
+		WordMatrix.foreignOrDomestic = _foreignOrDomestic;
+
+		DBCollection coll = MyDB.getDB().getCollection("newsItems_Events");
+		BasicDBObject query = new BasicDBObject();
+		query.append("nlp:entity.LOCATION", country);
 		DBCursor cursor = coll.find(query);
 
-		if (cursor.hasNext()) {
-			cursor.next();
-			Date start_date = convertStrToDate((cursor.curr().get("rssUpdFirst")).toString());
-			Date end_date = convertStrToDate((cursor.curr().get("rssUpdLast")).toString());
+		long results = coll.count(query);
+		int old_perc = 0;
 
-			String str = ((BasicDBObject) cursor.curr().get("keywords")).get("terms").toString();
-			String[] keywordsArr = str.substring(2, str.length() - 1).replace("\"", "").split(" , ");
+		HashSet<String> keywordSet = new HashSet<>();
+		int i = 0;
+		long time = 0;
+		Calendar minDate = Calendar.getInstance();
+		Calendar maxDate = Calendar.getInstance();
+		boolean firstIteration = true;
+		System.out.println("init");
 
-			keyWordSet = new HashSet<>(Arrays.asList(keywordsArr));
-			// keyWordSet.removeIf(p -> StringUtils.isNumericSpace(p));
-			Predicate<String> IS_NUMERIC = new Predicate<String>() {
-				@Override
-				public boolean test(String s) {
-					return StringUtils.isNumericSpace(s);
+		try {
+			while (cursor.hasNext()) {
+				cursor.next();
+				long t1 = System.currentTimeMillis();
+				BasicDBObject doc = (BasicDBObject) cursor.curr();
+
+				int new_perc = (int) (++i * 100 / (double) results);
+				if (new_perc > old_perc) {
+					old_perc = new_perc;
+					System.out.println(old_perc + "%. elapsed time: " + time / 1000.0 + " sec.");
 				}
-			};
+				String _id = doc.get("_id").toString();
+				String domain_name = "";
+				
+				try {
+					String url = ((BasicDBObject) doc.get("link")).get("href").toString();
+					domain_name = Utils.getDomainName(url);
 
-			keyWordSet.removeIf(IS_NUMERIC);
+					if (!domains.containsKey(domain_name)) {
 
-			return new WordMatrix(episode_id, start_date, end_date, keyWordSet);
+						String ip = java.net.InetAddress.getByName("www." + domain_name).getHostAddress();
+						String domain_country = Utils.getLookupService().getLocation(ip).countryName;
+						domains.put(domain_name, domain_country);
+					}
+					
+					if (domains.get(domain_name).equals(country)) {
+						// the web-site is domestic
+						if (foreignOrDomestic == Utils.FOREIGN) {
+							continue;
+						}
+
+					} else {
+						// the web-site is foreign
+						if (foreignOrDomestic == Utils.DOMESTIC) {
+							continue;
+						}
+
+						boolean mentionedCountry = ((BasicDBList) ((BasicDBObject) doc.get("nlp:entity"))
+								.get("LOCATION")).contains(country);
+						if (!mentionedCountry) {
+							continue;
+						}
+					}
+
+					Calendar publishDate = Utils.parseDateISO8601((doc.get("published")).toString());
+
+					if (firstIteration) {
+						minDate.setTimeInMillis(publishDate.getTimeInMillis());
+						maxDate.setTimeInMillis(publishDate.getTimeInMillis());
+						firstIteration = false;
+					} else {
+						if (publishDate.before(minDate)) {
+							minDate.setTimeInMillis(publishDate.getTimeInMillis());
+						}
+						if (publishDate.after(maxDate)) {
+							maxDate.setTimeInMillis(publishDate.getTimeInMillis());
+						}
+					}
+					String str = ((BasicDBObject) doc.get("keywords")).get("terms").toString();
+
+					String[] keywordsArr = str.substring(2, str.length() - 1) // remove
+																				// brackets
+							.replace("\"", "") // remove apostrophes
+							.split(" , ");
+
+					for (int k = 0; k < keywordsArr.length; k++) {
+						String keyword = keywordsArr[k];
+
+						// removes strings that are not alphanumeric
+						if (!StringUtils.isAlpha(keyword)) {
+							continue;
+						}
+
+						// removes stop-words
+						if (Utils.STOPWORDS.contains(keyword)) {
+							continue;
+						}
+
+						// remove the country from the keywords
+						if (keyword.contains(country.toLowerCase())) {
+							continue;
+						}
+
+						String lemma = Utils.lemmatize(keyword);
+						keywordSet.add(lemma);
+					}
+					event_keywords.put(_id, keywordSet);
+				} catch (NullPointerException | URISyntaxException | UnknownHostException | ParseException e) {
+					domains.get(domain_name);
+					e.printStackTrace();
+					continue;
+				} finally {
+					long t2 = System.currentTimeMillis();
+					time += t2 - t1;
+				}
+			}
+		} finally {
+			cursor.close();
 		}
-		return null;
+
+		try {
+			// exportCollection(domains, "domains");
+			Utils.exportCollection(keywordSet, "keywords");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new WordMatrix(minDate, maxDate, keywordSet);
 	}
 
 	public WordMatrix fill() {
-		BasicDBObject query = new BasicDBObject("episodeID", getEpisodeId());
+
 		DBCollection coll = MyDB.getDB().getCollection("newsItems_Events");
-		DBCursor cursor = coll.find(query);
-		List<String> keywords = getKeywords();
-		while (cursor.hasNext()) {
-			cursor.next();
-			for (String keyword : keywords) {
-				// String title = cursor.curr().get("title").toString();
-				Date date = convertStrToDate((cursor.curr().get("published")).toString());
-				int f = StringUtils.countMatches(cursor.curr().get("fullText").toString().toLowerCase(), keyword);
-				if (f > 0) {
-					insert(date, keyword, f);
-					// System.out.println("date= " + date + ", keyword= " +
-					// keyword + ", f= " + f);
+		int i = 0;
+		int old_perc = 0;
+		long time = 0;
+		long results = event_keywords.keySet().size();
+
+		System.out.println("fill");
+
+		for (String event_id : event_keywords.keySet()) {
+			try {
+				int new_perc = (int) (++i * 100 / (double) results);
+				if (new_perc > old_perc) {
+					old_perc = new_perc;
+					System.out.println(old_perc + "%. elapsed time: " + time / 1000.0 + " sec.");
 				}
+
+				long t1 = System.currentTimeMillis();
+				DBObject doc = findDocumentById(coll, event_id);
+
+				String url = ((BasicDBObject) doc.get("link")).get("href").toString();
+				String domain_name = Utils.getDomainName(url);
+
+				if (domains.get(domain_name).equals(country)) {
+					// the web-site is domestic
+					if (foreignOrDomestic == Utils.FOREIGN) {
+						continue;
+					}
+
+				} else {
+					// the web-site is foreign
+					if (foreignOrDomestic == Utils.DOMESTIC) {
+						continue;
+					}
+
+					boolean mentionedCountry = ((BasicDBList) ((BasicDBObject) doc.get("nlp:entity")).get("LOCATION"))
+							.contains(country);
+					if (!mentionedCountry) {
+						continue;
+					}
+				}
+
+				Calendar date = Utils.parseDateISO8601((doc.get("published")).toString());
+
+				String _id = doc.get("_id").toString();
+				HashSet<String> keywordSet = event_keywords.get(_id);
+
+				for (String keyword : keywordSet) {
+					int f = Utils.countMatches(doc.get("fullText").toString(), keyword);
+					if (f > 0) {
+						insert(date, keyword, f);
+					}
+				}
+
+				long t2 = System.currentTimeMillis();
+				time += t2 - t1;
+			} catch (ParseException | URISyntaxException e) {
+				e.printStackTrace();
+				continue;
 			}
 		}
 		return this;
 	}
 
-	@SuppressWarnings("deprecation")
 	public void exportTSV() throws IOException {
-
-		File file = new File(getEpisodeId());
+		System.out.println("starting export");
+		File file = new File(name);
 		BufferedWriter bw = new BufferedWriter(new FileWriter(file + ".tsv"));
+
 		ArrayList<String> keywordsToK = new ArrayList<>();
 		TreeSet<String> sortedKeywords = sortByFrequency();
 		int k = Math.min(NUM_OF_KEYWORDS, sortedKeywords.size());
+
 		bw.write("date\t");
 		for (int i = 0; i < k; i++) {
 			String key = sortedKeywords.pollFirst();
 			keywordsToK.add(key);
+			key = key.substring(0, 1).toUpperCase() + key.substring(1);
 
 			if (keywordsToK.size() != k)
 				bw.write(key + "\t");
@@ -249,14 +401,21 @@ public class WordMatrix {
 		}
 		bw.newLine();
 
-		Date dayAfterEnd = new Date(end_date.getYear(), end_date.getMonth(), end_date.getDate() + 1);
-		for (Date date = new Date(start_date.getTime()); date.before(dayAfterEnd); date.setDate(date.getDate() + 1)) {
-			bw.write(date.getYear() + 1900 + "" + date.getMonth() + 1 + "" + date.getDate() + "\t");
+		for (int row = 0; row < wordMatrix.length; row++) {
+			bw.write(getDateOfRow(row, dateFormatter) + "\t");
+
 			for (String keyword : keywordsToK) {
-				bw.write(getf(date, keyword) + "\t");
+				bw.write(getf(row, keyword) + "\t");
 			}
 			bw.newLine();
 		}
 		bw.close();
+	}
+
+	public DBObject findDocumentById(DBCollection collection, String id) {
+		BasicDBObject query = new BasicDBObject();
+		query.put("_id", new ObjectId(id));
+		DBObject dbObj = collection.findOne(query);
+		return dbObj;
 	}
 }
